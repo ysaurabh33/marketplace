dc1c56bb2bec2f33b5e4ddc3c8e40335c8d49890
ysaurabh33 <ysaurabh33@gmail.com>
2021-05-11 21:59:55 +0530

    config changes

--- config/amazon-mws.php
+++ config/amazon-mws.php
@@ -3,12 +3,12 @@
 return [
     'store' => [
         'store1' => [
-            'merchantId'       => 'A3DOJLQLY33AOC',
-            'marketplaceId'    => 'ATVPDKIKX0DER',
-            'keyId'            => 'AKIAIQXFDCTWCQLTT4YQ',
-            'secretKey'        => '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',
-            //'mwsAuthToken'     => '',
-            'amazonServiceUrl' => 'https://mws.amazonservices.com',
+            'merchantId'       => '',
+            'marketplaceId'    => '',
+            'keyId'            => '',
+            'secretKey'        => '',
+            'mwsAuthToken'     => '',
+            'amazonServiceUrl' => '',
             'muteLog'          => false,
         ],
     ],
